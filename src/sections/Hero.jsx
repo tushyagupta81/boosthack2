d4c13d26308bdfdba22cc2334c99a9a1656dcad2
Tushya Gupta <tushya@Tushyas-Laptop.local>
2024-07-19 14:44:22 +0530

    hero page work

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,14 @@
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="w-full bg-blue-800 pt-20 ">
+      <div className="flex flex-col w-80 text-white">
+        <h1 className="text-6xl p-10 pb-24">CoinED</h1>
+        <p className="text-4xl pl-10 pb-[50%] md:pb-[100%]">
+          learn.apply.repeat
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
