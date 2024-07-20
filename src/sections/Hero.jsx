0cbce9cf4e10cb9ace7cf65e1a78ebc29dc547a9
swarnim3103 <143250446+swarnim3103@users.noreply.github.com>
2024-07-20 16:45:30 +0530

    modified:   src/sections/About.jsx 	modified:   src/sections/Hero.jsx 	modified:   src/sections/Nav.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,10 +1,10 @@
 const Hero = () => {
   return (
-    <div className="w-full bg-blue-800 pt-20 ">
-      <div className="flex flex-col w-80 text-white">
-        <h1 className="text-6xl p-10 pb-24">CoinED</h1>
-        <p className="text-4xl pl-10 pb-[50%] md:pb-[100%]">
-          learn.apply.repeat
+    <div className=" bg-blue-700 pt-20 ">
+      <div className="flex flex-col w-80 text-white mt-30 ml-20 pt-20 ">
+        <h1 className="text-8xl p-10 pb-24 ">CoinED</h1>
+        <p className="text-6xl pl-10 pb-[50%] md:pb-[100%]">
+          Learn.Apply.Repeat
         </p>
       </div>
     </div>
--- src/sections/Nav.jsx
+++ src/sections/Nav.jsx
@@ -1,20 +1,19 @@
 const Nav = () => {
   return (
-    <header className="py-6 absolute z-10 w-full px-4 bg-blue-400">
-      <nav className="flex justify-between items-center">
+    <header className="fixed top-0 left-0 py-6  z-10 w-full px-4 bg-blue-700">
+      <nav className="flex justify-between items-center mx-auto">
         <div className="ml-5 cursor-pointer">
           <img
             src="https://cdn.dribbble.com/users/8322394/screenshots/16257129/media/52e02c3a2da5974e1f9d0d27de7ae5cb.jpg"
             alt="random logo"
-            width={60}
-            height={20}
+            width={80}
             className="rounded-full"
           />
         </div>
         <ul className="flex gap-16 text-xl font-sans max-md:hidden">
-          <li>Home</li>
-          <li>About us</li>
-          <li>Contact us</li>
+          <li className="cursor-pointer">Home</li>
+          <li className="cursor-pointer">About us</li>
+          <li className="cursor-pointer">Contact us</li>
         </ul>
         <div className="flex gap-5 mr-5 max-md:hidden text-xl cursor-pointer">
           <div>Login</div>
