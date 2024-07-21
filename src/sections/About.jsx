5cfbed1ae4fd44642196a553e7608ee884a2d394
swarnim3103 <143250446+swarnim3103@users.noreply.github.com>
2024-07-21 13:31:53 +0530

    modified:   src/sections/Footer.jsx

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,14 +1,8 @@
 const About = () => {
   return (
     <>
-    <div className="flex ml-5 mt-10 mr-5">
-      <b><h2 className="mr-10 text-blue-600 border-blue-600 border-2 rounded-lg p-2 text-3xl">About us</h2></b>
-      <p className="border-blue-600 border-2 rounded-lg p-4 text-lg">At CoinEd, our mission is to empower individuals with the knowledge and tools they need to make informed financial decisions. We believe that financial literacy is the key to achieving personal and professional goals, and we are committed to making high-quality financial education accessible to everyone.<br></br><br></br>
-      At CoinEd, we provide a wide range of resources designed to help you understand and navigate the complexities of personal finance. Our offerings include:<br></br>
-      Comprehensive Courses: Dive deep into topics like budgeting, investing, retirement planning, and more with our expertly crafted courses.<br></br>
-      Interactive quiz: Quiz after every lesson to jog memory and learn in effective ways.<br></br>
-      Tools: Utilize budgeting tools to take control and learn basics of planing of your finances.</p>
-      </div>
+      <h2>About us</h2>
+      <p>about info</p>
     </>
   );
 };
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,5 +1,15 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+  <div className="flex ml-5 mt-10 mr-5">
+  <b><h2 className="mr-10 text-blue-600 border-blue-600 border-2 rounded-lg p-2 text-3xl">Join_Us</h2></b>
+  <p className="border-blue-600 border-2 rounded-lg p-4 text-lg">Ready to take control of your financial future? Join CoinEd today and start your journey towards financial literacy and empowerment. Sign up or contact us to learn more about how we can help you achieve your financial goals.</p>
+  </div>
+  <footer>
+    
+  </footer>
+  </>
+);
 };
 
 export default Footer;
