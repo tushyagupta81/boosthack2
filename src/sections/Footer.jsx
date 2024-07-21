33227603ff9918726890482b11bcabdc30d4bf3b
swarnim3103 <143250446+swarnim3103@users.noreply.github.com>
2024-07-21 16:15:22 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/sections/Footer.jsx 	modified:   src/sections/Hero.jsx

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,15 +1,27 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGoogle, faInstagram, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
 const Footer = () => {
   return (
     <>
-  <div className="flex ml-5 mt-10 mr-5 max-md:flex-col max-md:mb-4">
-  <b><h2 className="mr-10 text-blue-600 p-2 text-3xl font-bold whitespace-nowrap">Join Us</h2></b>
-  <p className="border-l-2 rounded-sm p-4 text-lg">Ready to take control of your financial future? Join CoinEd today and start your journey towards financial literacy and empowerment. Sign up or contact us to learn more about how we can help you achieve your financial goals.</p>
-  </div>
-  <footer>
-    
-  </footer>
-  </>
-);
+      <div className="flex ml-5 mt-10 mr-5 max-md:flex-col max-md:mb-4">
+        <h2 className="mr-10 text-blue-600 p-2 text-3xl font-bold whitespace-nowrap">Join Us</h2>
+        <p className="border-l-2 rounded-sm p-4 text-lg">
+          Ready to take control of your financial future? Join CoinEd today and start your journey towards financial literacy and empowerment. Sign up or contact us to learn more about how we can help you achieve your financial goals.
+        </p>
+      </div>
+      <footer className="w-full text-white py-4">
+        <div className="container mx-auto text-center">
+          <div className="flex justify-center items-center space-x-2">
+            <FontAwesomeIcon icon={faEnvelope} size="2x" className="text-blue-400" />
+            <FontAwesomeIcon icon={faInstagram} size="2x" className="text-blue-400" />
+            <FontAwesomeIcon icon={faGithub} size="2x" className="text-blue-400" />
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,6 @@
 const Hero = () => {
   return (
-    <div className="bg-gradient-to-b from-white from-10% via-blue-400 via-30% to-blue-700 to-100%">
+    <div className="bg-gradient-to-b from-white from-4% via-blue-400 via-20% to-blue-800 to-100%">
       <div className="flex flex-col w-80 text-white ml-20 pt-10">
         <h1 className="text-8xl p-10 pb-24 drop-shadow-md mt-10">CoinED</h1>
         <p className="text-6xl pl-10 pb-[40%] md:pb-[80%] drop-shadow-md">
