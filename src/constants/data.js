fff40aaf15fe239c582c6c6115e94532021b97be
Shivrajsoni <shivrajsoni09022005@gmail.com>
2024-08-02 16:03:59 +0530

    added modules name and info

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,55 +1,59 @@
 const data = [
   {
     id: 1,
-    title: "post1",
+    title: "Stock Market Basics",
     info: [
-      "para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 ",
+     " start with this module if you are a market newbie.This module will help you understand the market basics including the IPO Markets,commonly used jargons,trading faTerminal,market events",
     ],
   },
   {
     id: 2,
-    title: "post2",
+    title: "Technical analysis",
     info: [
-      "para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 ",
-      "para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 ",
-      "para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 ",
+      "Technical Analysis (TA) helps in developing a point of view. In this module, we will discover the complex attributes, various patterns, indicators, and theories of TA that will help you as a trader to find upright trading opportunities in the market."
     ],
   },
   {
     id: 3,
-    title: "post3",
+    title: "Fundamental Analysis",
     info: [
-      "para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 ",
+      "The Fundamental Analysis (FA) module explores Equity research by reading financial statements and annual reports, calculating and analyzing Financial Ratios, and evaluating the intrinsic value of a stock to find long-term investing opportunities."
     ],
   },
   {
     id: 4,
-    title: "post4",
+    title: "Futures Trading",
     info: [
-      "para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 ",
-      "para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 ",
-      "para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 ",
-      "para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 ",
+      "Futures Trading is a segment of the derivatives market. This module covers the various intricacies involved in undergoing a futures trade, including margins, leverages, pricing, etc. It also discusses the use of derivatives for hedging purposes."
     ],
   },
   {
     id: 5,
-    title: "post5",
+    title: "Markets and Taxation",
     info: [
-      "para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 ",
-      "para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 ",
+      "As a trader in India, you should be informed of the taxes applicable to your investments and trades. This module outlines essential topics like calculating your turnover, preparing balance sheet and P&L statements, and filing your Income Tax Returns."
     ],
   },
   {
     id: 6,
-    title: "post6",
+    title: "Currency, Commodity, and Government Securities",
     info: [
-      "para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 ",
-      "para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 ",
-      "para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 ",
-      "para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 para1 ",
+      "Interest rates, forex rates, and inflation impact each other and commodity prices. Commodity prices impact each other, too. This module discusses trading in currency and commodity derivatives and Government Securities (GSec) in the Indian markets."
     ],
   },
+  {
+    id:7,
+    title:"Risk Management and Trading Psychology",
+    info:[
+      "Trading and investing are subject to risks. Our emotions also influence our choices in the market and could aggravate risks. This module discusses risk and risk management tools along with the psychology required for sustaining in the markets."
+    ],
+  },
+  {
+    id:8,
+    title:'InnerWorth - Mind over markets',
+    info:["Originally a collection of newsletters on trading psychology published between 2002 and 2007 by Marketwise, a US stock broking firm, 'Innerworth - Mind over Markets' will guide you and prepare you psychologically to become a rational trader."],
+
+  }
 ];
 
 export default data;
