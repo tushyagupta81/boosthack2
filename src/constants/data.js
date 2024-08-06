db66f0e139ecfd26eb27144b2e5ab085df7aaed2
Tushya <tushya007@gmail.com>
2024-08-06 11:39:02 +0530

    template for data

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,64 +1,50 @@
-const data = [
-  // {
-  //   id: 1,
-  //   title: "Stock Market Basics",
-  //   info: {
-  //     id:1,
-  //     title:"titel",
-  //     info:[
-  //       "kw"
-  //     ]
-  //   }
-  // },
+const chapters = [
+  {
+    id: 1,
+    title: "Chapter title",
+    info: ["basic info on the chapter in 1-2 paras", "this is para 2"],
+  },
   {
     id: 2,
-    title: "Technical analysis",
-    info: [
-      "Technical Analysis (TA) helps in developing a point of view. In this module, we will discover the complex attributes, various patterns, indicators, and theories of TA that will help you as a trader to find upright trading opportunities in the market.",
-    ],
-  },
-  {
-    id: 3,
-    title: "Fundamental Analysis",
-    info: [
-      "The Fundamental Analysis (FA) module explores Equity research by reading financial statements and annual reports, calculating and analyzing Financial Ratios, and evaluating the intrinsic value of a stock to find long-term investing opportunities.",
-    ],
-  },
-  {
-    id: 4,
-    title: "Futures Trading",
-    info: [
-      "Futures Trading is a segment of the derivatives market. This module covers the various intricacies involved in undergoing a futures trade, including margins, leverages, pricing, etc. It also discusses the use of derivatives for hedging purposes.",
-    ],
-  },
-  {
-    id: 5,
-    title: "Markets and Taxation",
-    info: [
-      "As a trader in India, you should be informed of the taxes applicable to your investments and trades. This module outlines essential topics like calculating your turnover, preparing balance sheet and P&L statements, and filing your Income Tax Returns.",
-    ],
-  },
-  {
-    id: 6,
-    title: "Currency, Commodity, and Government Securities",
-    info: [
-      "Interest rates, forex rates, and inflation impact each other and commodity prices. Commodity prices impact each other, too. This module discusses trading in currency and commodity derivatives and Government Securities (GSec) in the Indian markets.",
-    ],
-  },
-  {
-    id: 7,
-    title: "Risk Management and Trading Psychology",
-    info: [
-      "Trading and investing are subject to risks. Our emotions also influence our choices in the market and could aggravate risks. This module discusses risk and risk management tools along with the psychology required for sustaining in the markets.",
-    ],
-  },
-  {
-    id: 8,
-    title: "InnerWorth - Mind over markets",
-    info: [
-      "Originally a collection of newsletters on trading psychology published between 2002 and 2007 by Marketwise, a US stock broking firm, 'Innerworth - Mind over Markets' will guide you and prepare you psychologically to become a rational trader.",
-    ],
+    title: "2",
+    info: [],
   },
 ];
 
-export default data;
+const submodules = [
+  {
+    id: 1,
+    // id of the chapter it belongs to
+    chapterId: 1,
+    title: "submodules title",
+    info: ["intro to the submodules in 1-2 paras", "para 2"],
+  },
+  {
+    id: 2,
+    // id of the chapter it belongs to
+    chapterId: 1,
+    title: "submodule 2",
+    info: [],
+  },
+];
+
+const lessons = [
+  {
+    id: 1,
+    // id of the submodule it belongs to
+    submoduleId: 1,
+    // id of the chapter it belongs to
+    chapterId: 1,
+    info: ["information in detail", "multiple paras"],
+  },
+  {
+    id: 2,
+    // id of the submodule it belongs to
+    submoduleId: 1,
+    // id of the chapter it belongs to
+    chapterId: 1,
+    info: ["information in detail", "multiple paras"],
+  },
+];
+
+export { chapters, submodules, lessons };
