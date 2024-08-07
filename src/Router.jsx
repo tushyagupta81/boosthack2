e372d7bbe32feadff52741eb84fcdca867def393
Pratishtha <pratishtha729@gmail.com>
2024-08-07 06:53:07 +0530

    tools page added

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { App, Signin, Dashboard, Signup, Post } from "./pages/index.js";
+import { App, Signin, Dashboard, Signup, Post, Tools} from "./pages/index.js";
 
 function Router() {
   return (
@@ -10,6 +10,7 @@ function Router() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/post/:id" element={<Post />} />
+        <Route path="/tools" element={<Tools />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,6 @@ import Signin from "./Signin.jsx";
 import Dashboard from "./Dashboard.jsx";
 import Signup from "./Signup.jsx";
 import Post from "./Post.jsx";
+import Tools from "./Tools.jsx";
 
-export { App, Signin, Dashboard, Signup, Post };
+export { App, Signin, Dashboard, Signup, Post, Tools};
